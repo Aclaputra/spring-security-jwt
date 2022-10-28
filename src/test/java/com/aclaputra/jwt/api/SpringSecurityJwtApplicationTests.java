25ba4fc1302841fb2b02f3c96b0f7615ce627540
aclaputra <muhamadacla22@gmail.com>
2022-10-28 18:05:14 +0700

    feat: initial commit

---
+++ src/test/java/com/aclaputra/jwt/api/SpringSecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aclaputra.jwt.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
